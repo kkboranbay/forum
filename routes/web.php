cb60c7458fa24c27ea8f4b443933f90d0ad2820b
kkboranbay <kkboranbay@gmail.com>
2020-02-16 17:00:09 +0600

    Lesson 11

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,15 @@ Route::get('/', function () {
 });
 
 
-Route::resource('threads', 'ThreadController');
-Route::get('/threads', 'ThreadController@index');
-Route::get('/threads/{channel}/{thread}', 'ThreadController@show');
-Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
-Route::post('/threads', 'ThreadController@store');
+//Route::resource('threads', 'ThreadController');
+
+Route::get('home', 'HomeController@index');
+Route::get('threads', 'ThreadController@index');
+Route::get('threads/create', 'ThreadController@create');
+Route::get('threads/{channel}/{thread}', 'ThreadController@show');
+Route::post('threads', 'ThreadController@store');
+Route::post('threads/{channel}/{thread}/replies', 'ReplyController@store');
+Route::get('threads/{channel}', 'ThreadController@index');
 
 Auth::routes();
 
