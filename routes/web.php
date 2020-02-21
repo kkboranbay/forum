83c83a7a513e6b67a57098e06ac3bd7242f036c9
kkboranbay <kkboranbay@gmail.com>
2020-02-21 22:55:51 +0600

    Lesson 18

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('threads', 'ThreadController@store');
 Route::post('threads/{channel}/{thread}/replies', 'ReplyController@store');
 Route::get('threads/{channel}', 'ThreadController@index');
+Route::post('replies/{reply}/favorites', 'FavoriteController@store');
 
 Auth::routes();
 
---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Favorite;
+use App\Reply;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FavoritesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function guests_cannot_favorite_anything()
+    {
+        $this->post('replies/1/favorites')
+            ->assertRedirect('login');
+
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        $this->post('replies/' . $reply->id . '/favorites');
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    /** @test */
+    public function an_authenticated_user_may_only_favorite_a_reply_once()
+    {
+        $this->withoutExceptionHandling();
+        $this->signIn();
+
+        $reply = create(Reply::class);
+
+        try {
+            $this->post('replies/' . $reply->id . '/favorites');
+            $this->post('replies/' . $reply->id . '/favorites');
+        } catch (\Exception $e) {
+            $this->fail('Did not expect to insert the same record set twice.');
+        }
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
