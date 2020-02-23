a7958f2319a585737ac595f685a61ee30e081e29
kkboranbay <kkboranbay@gmail.com>
2020-02-23 13:54:28 +0600

    Lesson 24

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                @foreach($threads as $thread)
+                @forelse($threads as $thread)
                     <div class="card mt-3">
                         <div class="card-header">
                             <div class="level">
@@ -25,7 +25,9 @@
                             <hr>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <p>There is no relevant results at this time</p>
+                @endforelse
             </div>
         </div>
     </div>
