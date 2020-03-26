88bc206e12d885261831f93a405c7cfb9e0db03b
kkboranbay <kkboranbay@gmail.com>
2020-03-26 19:17:23 +0600

    Lesson 46

--- app/Thread.php
+++ app/Thread.php
@@ -50,13 +50,17 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
-        $this->subscriptions
-            ->filter(function ($subscription) use ($reply) {
-                return $subscription->user_id != $reply->user_id;
-            })
-            ->each->notify($reply);
+        $this->notifySubscribers($reply);
 
         return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
+        $this->subscriptions
+            ->where('user_id', '!=', $reply->user_id)
+            ->each
+            ->notify($reply);
     }
 
     /**
