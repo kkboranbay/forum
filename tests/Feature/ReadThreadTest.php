96046622b62843551bda6fb6ff2372e8af996829
kkboranbay <kkboranbay@gmail.com>
2020-04-09 21:25:57 +0600

    Lesson 62. Basic View Tweaks

--- tests/Feature/ReadThreadTest.php
+++ tests/Feature/ReadThreadTest.php
@@ -69,18 +69,19 @@ class ReadThreadTest extends TestCase
 
         $response = $this->getJson('threads?popular=1')->json();
 
-        $this->assertEquals([3,2,0], array_column($response, 'replies_count'));
+        $this->assertEquals([3,2,0], array_column($response['data'], 'replies_count'));
     }
 
     /** @test */
     public function a_user_can_filter_threads_by_those_that_are_unanswered()
     {
         $thread = create('App\Thread');
+
         create('App\Reply', ['thread_id' => $thread->id]);
 
         $response = $this->getJson('threads?unanswered=1')->json();
 
-        $this->assertCount(1, $response);
+        $this->assertCount(1, $response['data']);
     }
 
     /** @test */
